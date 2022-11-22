b567a1c7b550212502c9583ec5a53826dc25d46c
MikeChara <mikejchara@gmail.com>
2022-11-22 18:24:08 +0000

    bunch of routes set up

--- Routes/posts.js
+++ Routes/posts.js
@@ -6,3 +6,43 @@ postsRouter.get("/", async function (req, res) {
   const allPosts = await getAllPosts();
   res.json({ success: true, payload: allPosts });
 });
+
+//get single post by ID, and all comments
+postsRouter.get("/:id", async function (req, res) {
+  const post = await getPost(req);
+  res.json({ success: true, payload: post });
+});
+
+//post a post.
+postsRouter.post("/", async function (req, res) {
+  const data = req.body;
+  const newPost = await createPost(data);
+  res.json({ success: true, payload: newPost });
+});
+
+//post a comment.
+postsRouter.post("/:id", async function (req, res) {
+  const data = req.body;
+  const newComment = await createComment(data);
+  res.json({ success: true, payload: newComment });
+});
+
+//patch a post by ID.
+postsRouter.patch("/:id", async function (req, res) {
+  const data = req.body;
+  const updatedPost = await updatePost(data);
+  res.json({ success: true, payload: updatedPost });
+});
+
+//patch a comment by ID.
+postsRouter.patch("/:id", async function (req, res) {
+  const data = req.body;
+  const updatedComment = await updateComment(data);
+  res.json({ success: true, payload: updatedComment });
+});
+
+//delete a comment by ID.
+postsRouter.delete("/:id", async function (req, res));
+//delete a comment by ID.
+//getposts by title search VERSION 2
+//
