38ea61e4c7510a3540903887997ccf0ec00c7534
MikeChara <mikejchara@gmail.com>
2022-11-24 11:30:00 +0000

    set up new db

---
+++ db/dbCreationScript.js
@@ -0,0 +1,45 @@
+// import query from "./index.js";
+
+// export async function createAllTables() {
+//   return await query(
+//     `CREATE TABLE IF NOT EXISTS users (
+//       id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//       duck_name VARCHAR(60)
+//     );
+
+//     INSERT INTO users
+//       (duck_name)
+//     VALUES
+//     (
+//       'Blue duck'
+//   ),
+//   (
+//       'Dave duck'
+//   ),
+//   (
+//       'Brown duck'
+//   ),
+//   (
+//       'Orange duck'
+//   ),
+//   (
+//       'Yellow duck'
+//   ),
+//   (
+//       'Red duck'
+//   ),
+//   (
+//       'Blue duck'
+//   ),
+//   (
+//       'Green duck'
+//   ),
+//   (
+//       'Purple duck'
+//     ;
+
+//     `
+//   );
+// }
+
+// createAllTables();
